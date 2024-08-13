c93f32d34a617655bd08c8a8fc9b078b78810173
kazuma kikuya <61053639+kazumax75@users.noreply.github.com>
2024-08-13 09:54:56 +0900

    うごきそう

---
+++ video_predictor_example.py
@@ -0,0 +1,105 @@
+import os
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+from sam2.build_sam import build_sam2_video_predictor
+
+# select the device for computation
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+print(f"using device: {device}")
+
+if device.type == "cuda":
+    # use bfloat16 for the entire notebook
+    torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+    if torch.cuda.get_device_properties(0).major >= 8:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+elif device.type == "mps":
+    print(
+        "\nSupport for MPS devices is preliminary. SAM 2 is trained with CUDA and might "
+        "give numerically different outputs and sometimes degraded performance on MPS. "
+        "See e.g. https://github.com/pytorch/pytorch/issues/84936 for a discussion."
+    )
+
+
+# ここから
+sam2_checkpoint = "C:\\Users\\morea\\Documents\\pg\\python\\sam2\\segment-anything-2\\checkpoints\\sam2_hiera_large.pt"
+model_cfg =       "C:\\Users\\morea\\Documents\\pg\\python\\sam2\\segment-anything-2\\sam2_configs\\sam2_hiera_l.yaml"
+
+predictor = build_sam2_video_predictor(model_cfg, sam2_checkpoint, device=device)
+
+
+def show_mask(mask, ax, obj_id=None, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        cmap = plt.get_cmap("tab10")
+        cmap_idx = 0 if obj_id is None else obj_id
+        color = np.array([*cmap(cmap_idx)[:3], 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_points(coords, labels, ax, marker_size=200):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
+    
+    
+# `video_dir` a directory of JPEG frames with filenames like `<frame_index>.jpg`
+video_dir = "C:\\Users\\morea\\Documents\\pg\\python\\sam2\\segment-anything-2\\video"
+
+# scan all the JPEG frame names in this directory
+frame_names = [
+    p for p in os.listdir(video_dir)
+    if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]
+]
+frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
+
+# take a look the first video frame
+frame_idx = 0
+plt.figure(figsize=(9, 6))
+plt.title(f"frame {frame_idx}")
+plt.imshow(Image.open(os.path.join(video_dir, frame_names[frame_idx])))
+
+print(frame_names)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
